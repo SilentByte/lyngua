ba0e1b24209648bdbd3388c9b0fe78b9398ab3e5
Rico A. Beti <rico.beti@silentbyte.com>
2021-02-24 01:14:15 +0800

    Update view based on current playback state

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -12,12 +12,24 @@
                     <v-col cols="12">
                         <v-card outlined
                                 :height="playerHeight">
-                            <youtube ref="youtube"
+                            <v-skeleton-loader v-if="!transcription"
+                                               type="image"
+                                               class="image-skeleton-loader"
+                                               height="100%" />
+
+                            <youtube v-else
+                                     ref="youtube"
                                      nocookie
-                                     class="youtube-iframe"
                                      video-id="LseK5gp66u8"
                                      width="100%"
-                                     height="100%" />
+                                     height="100%"
+                                     @ready="onPlayerReady"
+                                     @ended="onPlayerEnded"
+                                     @playing="onPlayerPlaying"
+                                     @paused="onPlayerPaused"
+                                     @buffering="onPlayerBuffering"
+                                     @cued="onPlayerCued"
+                                     @error="onPlayerError" />
                         </v-card>
                     </v-col>
                     <v-col cols="12">
@@ -47,7 +59,11 @@
 
                     <!-- Keep on one line because whitespace is relevant here. -->
                     <template v-else v-for="w in transcription.words">
-                        <span :key="w.index" :data-index="w.index" class="word">{{ w.text }}</span>
+                        <span :key="w.index"
+                              :ref="`word-${w.index}`"
+                              :class="['word', currentWordIndex === w.index ? 'active' : '']"
+                              @click="onWordClick(w)"
+                        >{{ w.text }}</span>
                         <span :key="`${w.index}-s`" class="space">{{ " " }}</span>
                     </template>
                 </v-card>
@@ -70,10 +86,15 @@ import rangy from "rangy";
 import { getModule } from "vuex-module-decorators";
 import {
     AppModule,
-    ITranscription,
+    ITranscription, IWord,
 } from "@/store/app";
 
-// TODO: Make playback speed changeable.
+const PLAYER_TICK_INTERVAL = 100;
+
+// TODO: - Make playback speed changeable
+//       - Auto-scroll current word into view
+//       - When clicked/hovered on word, show FABs like 'go to (seek)', 'play', 'record'.
+//       - When words are selected, show FABs, e.g. 'play'. Then only play exactly this part.
 
 @Component
 export default class HomeView extends Vue {
@@ -84,9 +105,30 @@ export default class HomeView extends Vue {
     private playerHeight = 400;
     private controlsHeight = 400;
     private transcriptHeight = 400;
-    private selectionChangedNativeEvent!: () => void;
+    private nativeSelectionChangedEvent!: () => void;
+    private nativePlayerTickInterval!: number;
 
     private transcription: ITranscription | null = null;
+    private currentWordIndex: number | null = null;
+
+    private get player(): any {
+        return (this.$refs.youtube as any).player;
+    }
+
+    private findWordByOffset(offset: number): IWord | null {
+        if(!this.transcription) {
+            return null;
+        }
+
+        const words = this.transcription.words;
+        for(let i = 0; i < words.length; i += 1) {
+            if(words[i].offset > offset) {
+                return words[i - 1];
+            }
+        }
+
+        return null;
+    }
 
     private onResize() {
         const height = window.innerHeight - 90;
@@ -111,15 +153,60 @@ export default class HomeView extends Vue {
         console.log("-----------------------------");
     }
 
+    private onWordClick(word: IWord) {
+        console.log(word);
+    }
+
+    private onPlayerReady() {
+        console.log("onPlayerReady");
+    }
+
+    private onPlayerEnded() {
+        console.log("onPlayerEnded");
+    }
+
+    private onPlayerPlaying() {
+        console.log("onPlayerPlaying");
+    }
+
+    private onPlayerPaused() {
+        console.log("onPlayerPaused");
+    }
+
+    private onPlayerBuffering() {
+        console.log("onPlayerBuffering");
+    }
+
+    private onPlayerCued() {
+        console.log("onPlayerCued");
+    }
+
+    private onPlayerError() {
+        console.log("onPlayerError");
+    }
+
+    private async onPlayerTick() {
+        const offset = await this.player.getCurrentTime();
+        const word = this.findWordByOffset(offset);
+
+        if(word) {
+            this.currentWordIndex = word.index;
+        }
+    }
+
     async mounted(): Promise<void> {
-        this.selectionChangedNativeEvent = () => this.onSelectionChanged();
-        document.addEventListener("selectionchange", this.selectionChangedNativeEvent);
+        this.nativeSelectionChangedEvent = () => this.onSelectionChanged();
+        this.nativePlayerTickInterval = setInterval(() => this.onPlayerTick(), PLAYER_TICK_INTERVAL) as unknown as number;
+
+        document.addEventListener("selectionchange", this.nativeSelectionChangedEvent);
 
         this.transcription = await this.app.doTranscribe({youTubeVideoId: "LseK5gp66u8"});
+
     }
 
     beforeDestroy(): void {
-        document.removeEventListener("selectionchange", this.selectionChangedNativeEvent);
+        document.removeEventListener("selectionchange", this.nativeSelectionChangedEvent);
+        clearInterval(this.nativePlayerTickInterval);
     }
 }
 
@@ -141,7 +228,7 @@ export default class HomeView extends Vue {
 
     .word {
         padding: 4px;
-        border: 2px dotted transparent;
+        border: 2px solid transparent;
         border-radius: 4px;
 
         cursor: pointer;
@@ -149,6 +236,17 @@ export default class HomeView extends Vue {
         &:hover {
             border-color: $primary-color;
         }
+    }
+
+    .word.active {
+        color: $primary-color;
+        border-color: $primary-color;
+    }
+}
+
+.image-skeleton-loader {
+    & ::v-deep .v-skeleton-loader__image {
+        height: 100%;
     }
 }
 
