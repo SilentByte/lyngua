e8e497f3f7547bc54c6c308dead2f3a79a445e8e
Rico A. Beti <rico.beti@silentbyte.com>
2021-03-14 11:35:44 +0800

    Add loading messages

--- app/src/views/dialogs/AppBlockingDialog.vue
+++ app/src/views/dialogs/AppBlockingDialog.vue
@@ -5,24 +5,29 @@
 
 <template>
     <v-dialog persistent
-              overlay-color="#F00"
+              overlay-opacity="0.8"
               content-class="elevation-0"
               style="z-index: 99999"
-              :value="app.isAppBlocked||1">
+              :value="app.isAppBlocked">
         <v-card color="transparent"
                 class="elevation-0">
-            <v-layout fill-height
+            <v-layout fill-height column
                       class="ma-5 pa-5 justify-center align-center">
                 <v-progress-circular indeterminate
                                      class="pa-4"
                                      size="60"
                                      width="5"
                                      color="white" />
+
+                <v-chip class="mt-8">
+                    {{ messages[messageIndex] }}
+                </v-chip>
             </v-layout>
         </v-card>
     </v-dialog>
 </template>
 
+<!--suppress JSUnusedGlobalSymbols -->
 <script lang="ts">
 
 import {
@@ -36,6 +41,27 @@ import { AppModule } from "@/store/app";
 @Component
 export default class AppBlockingDialog extends Vue {
     private readonly app = getModule(AppModule);
+
+    private nativeInterval = 0;
+
+    private messageIndex = 0;
+    private messages = [
+        "Crunching some numbers…",
+        "Performing voice recognition…",
+        "Hiring a professional translator…",
+        "Improving your learning experience…",
+    ];
+
+    mounted(): void {
+        this.nativeInterval = setInterval(
+            () => this.messageIndex = Math.floor(Math.random() * this.messages.length),
+            4000,
+        ) as unknown as number;
+    }
+
+    beforeDestroy(): void {
+        clearInterval(this.nativeInterval);
+    }
 }
 
 </script>
